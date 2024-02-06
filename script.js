ee906d0184464c64e9cac09fe53e3f7cfadc2e37
Masud Alam <123488782+masudalamktr54@users.noreply.github.com>
2024-02-07 02:04:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,19 @@ let swiper = new Swiper(".mySwiper", {
 	},
 });
 
+
+// +++++++* side bar js code *+++++++++++++
+document.querySelector(".sidebar").style.display = "none"
+document.querySelector(".hamburger").addEventListener("click", () => {
+	document.querySelector(".sidebar").style.display = "inline"
+
+	document.querySelector(".hamburger").style.display = "none"
+})
+
+document.querySelector(".cross").addEventListener("click", () => {
+	document.querySelector(".hamburger").style.display = "inline"
+
+	document.querySelector(".sidebar").style.display = "none"
+})
+// +++++++* end side bar js code *+++++++++++++
+
