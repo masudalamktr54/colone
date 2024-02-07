a1bbcb574f60d617449b5558ff6f3d639fe70686
Masud Alam <123488782+masudalamktr54@users.noreply.github.com>
2024-02-08 02:31:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-let swiper = new Swiper(".mySwiper", {
-	spaceBetween: 10,
-	// effect: "fade",
-	// effect: "creative",
-	loop: true,
-	freeMode: true,
-    watchSlidesProgress: true,
-
-	autoplay: {
-	delay: 2500,
-	disableOnInteraction: false,
-},
-
-	navigation: {
-	nextEl: ".swiper-button-next",
-	prevEl: ".swiper-button-prev",
-	},
-	pagination: {
-	el: ".swiper-pagination",
-	clickable: true,
-	},
-});
-
-
 // +++++++* side bar js code *+++++++++++++
 document.querySelector(".sidebar").style.display = "none"
 document.querySelector(".hamburger").addEventListener("click", () => {
